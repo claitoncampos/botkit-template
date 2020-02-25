38f26190c9a65846d3346daa22a20442b52f0fda
Claiton Campos <claitoncampos@gmail.com>
2020-02-25 09:28:02 -0300

    Interactive bot

--- features/sam_bot.js
+++ features/sam_bot.js
@@ -4,61 +4,25 @@
 // bot: Em que posso ajudar?
 // user: Terá algumas opções para interação com o bot
 //
-module.exports = function (controller) {
+const { BotkitConversation } = require( 'botkit' );
 
-    controller.hears(['activities'], 'direct_message,direct_mention', function (bot, message) {
+module.exports = function ( controller) {
 
-        bot.startConversation(message, function (err, convo) {
+    const convo = new BotkitConversation( 'sam_chat', controller);
 
-            var question = "Here are a few proposed DevNet activities:";
-            question += "<br/> `1)` Verificar o status da rede (**rede**)";
-            question += "<br/> `2)` Total de pessoas conectadas no Wi-Fi (**wifi**)";
-            question += "<br/> `3)` Temperatuda da loja (**loja**)";
-            question += "<br/> `4)` Iluminacao da loja (**iluminacao**)";
-            question += "\n\O que voce gostaria de fazer?<br/>_(Digite um numero, escreva a **palavra chave** ou" +
-                "apenas escreva cancel)_";
-            convo.ask(question, [
-                {
-                    pattern: "1|rede|status",
-                    callback: function (response, convo) {
-                        convo.say("Um momento por favor, estou coletando todas as informacoes");
-                        convo.next();
-                    },
-                }
-                , {
-                    pattern: "2|wifi|pessoas",
-                    callback: function (response, convo) {
-                        convo.say("Informacoes sobre a saude da rede wi-fi e quantidade de pessoas conectadas");
-                        convo.next();
-                    },
-                }
-                , {
-                    pattern: "3|temperatura",
-                    callback: function (response, convo) {
-                        convo.say("Esta eh a temperatura atual");
-                        convo.next();
-                    },
-                }
-                , {
-                    pattern: "cancel|cancelar|stop",
-                    callback: function (response, convo) {
-                        convo.say("Ok, cancelando...");
-                        convo.next();
-                    },
-                }
-                , {
-                    default: true,
-                    callback: function (response, convo) {
-                        convo.gotoThread('bad_response');
-                    }
-                }
-            ]);
+    convo.say( 'Ola, eu sou o S.A.M, seu assistente pessoal' );
+    convo.ask( 'O que voce gostaria de verificar?',
+        async (answer, convo, bot) => {},
+        'stated_sam'
+    );
 
-            // Bad response
-            convo.addMessage({
-                text: "Desculpe, Eu nao entendi.",
-                action: 'default',
-            }, 'bad_response');
-        });
+    controller.addDialog( convo );
+
+    controller.hears( 'sam', 'message,direct_message', async (bot, message) => {
+
+        await bot.beginDialog( 'sam_chat' );
     });
-};
+
+    controller.commandHelp.push( { command: 'sam', text: 'Pick a Favorite sam (Botkit conversations)' } );
+
+}
